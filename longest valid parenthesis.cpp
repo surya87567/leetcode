715a2844573ddfc086d13e9c65e12330f394b34c
surya87567 <singhsuryapratap058@gmail.com>
2025-02-22 12:57:00 +0530

    longest valid prenthesis

---
+++ longest valid parenthesis.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        stack<int> st;
+        st.push(-1);
+        int maxLength = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(') {
+                st.push(i);
+            } else {
+                st.pop();
+                if (!st.empty()) {
+                    maxLength = max(maxLength, i - st.top());
+                } else {
+                    st.push(i);
+                }
+            }
+        }
+        return maxLength;
+    }
+};
