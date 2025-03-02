dbfdaecb6eff3ffb4df034030a848ad3e1a9cb78
surya87567 <singhsuryapratap058@gmail.com>
2025-03-02 19:46:32 +0530

    leetcode 2570

---
+++ leetcode_2570.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
+        map<int, int> mp;
+        for (int i = 0; i < nums1.size(); i++) mp[nums1[i][0]] += nums1[i][1];
+        for (int i = 0; i < nums2.size(); i++) mp[nums2[i][0]] += nums2[i][1];
+        vector<vector<int>> res;
+        for (auto it = mp.begin(); it != mp.end(); it++) res.push_back({it->first, it->second});
+        return res;
+    }
+};
